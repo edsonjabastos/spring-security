8c9307998c4784f970049d93f365971e6286869d
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2023-09-19 11:25:46 -0300

    feat: first routes.

--- src/main/java/io/github/edsonjabastos/springsecurity/SpringSecurityApplication.java
+++ src/main/java/io/github/edsonjabastos/springsecurity/SpringSecurityApplication.java
@@ -2,12 +2,26 @@ package io.github.edsonjabastos.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringSecurityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+
+	@RestController
+	class HttpController {
+		@GetMapping("/public")
+		String publicRoute() {
+			return "<h1>This is a public route, feel free to look around 🔓 </h1>";
+		}
+		@GetMapping("/private")
+		String privateRoute() {
+			return "<h1>This is a private route, only authorized requisitions con access... 🔒 </h1>";
+		}
 	}
 
 }
