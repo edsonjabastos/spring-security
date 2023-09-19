903b954b4a1fcb506f16a72721ea3cb4814add11
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2023-09-19 17:10:16 -0300

    feat: show user data on private route

--- src/main/java/io/github/edsonjabastos/springsecurity/SpringSecurityApplication.java
+++ src/main/java/io/github/edsonjabastos/springsecurity/SpringSecurityApplication.java
@@ -2,6 +2,8 @@ package io.github.edsonjabastos.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,9 +20,17 @@ public class SpringSecurityApplication {
 		String publicRoute() {
 			return "<h1>This is a public route, feel free to look around 🔓 </h1>";
 		}
+
 		@GetMapping("/private")
-		String privateRoute() {
-			return "<h1>This is a private route, only authorized requisitions con access... 🔒 </h1>";
+		String privateRoute(@AuthenticationPrincipal OidcUser principal) {
+			return String.format("""
+						<h1>This is a private route, only authorized requisitions con access... 🔒 </h1>
+						<h3>Principal: %s </h3>
+						<h3>Email attribute: %s </h3>
+						<h3>Authorities: %s </h3>
+						<h3>JWT: %s </h3>
+					""", principal, principal.getAttribute("email"), principal.getAuthorities(),
+					principal.getIdToken().getTokenValue());
 		}
 	}
 
